8ce8fa9f4c20887f75e6f7987b72192754c62bd3
shahid <shahid.ice.nstu@gmail.com>
2020-03-11 14:43:22 +0600

    Completed library project

--- library_project/settings.py
+++ library_project/settings.py
@@ -38,8 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party
+    'rest_framework',
+
     # local apps
     'books.apps.BooksConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
